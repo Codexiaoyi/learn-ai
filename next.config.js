13a4e6afbb20fd717b363459f1dc7eda81229fcb
glace lin <glace.lin@zoom.us>
2025-02-13 18:25:23 +0800

    fix eslint

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
   transpilePackages: [
     '@ant-design/icons',
     '@ant-design/cssinjs',
