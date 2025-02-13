c0a6607ab97d348bcc863a4f8fc8cd71e0e26743
glace lin <glace.lin@zoom.us>
2025-02-13 19:30:10 +0800

    update

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,7 @@
-// postcss.config.mjs
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+module.exports = {
+  plugins: [
+    'tailwindcss',  // 确保这里引用的是 tailwindcss 而不是 @tailwindcss/postcss
+    'autoprefixer',
+    '@tailwindcss/postcss',  // 引入 @tailwindcss/postcss 插件
+  ],
 };
